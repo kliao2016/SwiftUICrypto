7524eaa5169034e2a51f235d123940d85ec15db6
Kevin Liao <kliao2016@gmail.com>
2021-11-26 22:36:48 -0700

    Add networking error

--- SwiftUICrypto/Services/Utilities/NetworkManager.swift
+++ SwiftUICrypto/Services/Utilities/NetworkManager.swift
@@ -9,18 +9,35 @@ import Foundation
 import Combine
 
 final class NetworkManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(let url): return "[🔥] Bad response from URL: \(url)"
+            case .unknown: return "[⚠️] Unknown error ocurred"
+            }
+        }
+    }
+    
     static func download(url: URL) -> AnyPublisher<Data, Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
             .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
+                try handleURLResponse(output: output, url: url)
             }
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url)
+        }
+        return output.data
     }
     
     static func handleCompletion(completion: Subscribers.Completion<Error>) {
